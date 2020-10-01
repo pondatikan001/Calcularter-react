35ba9afad082685c83bf62cf1dd72d6fe01220b9
pondatikan001 <69183348+pondatikan001@users.noreply.github.com>
2020-10-01 16:18:39 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./componet/Calculatenumber";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+}
